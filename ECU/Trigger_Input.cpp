2f0332af662462bd1df0a41d0c5068eaf16c56b8
PaintX <remy.arnaudin@wanadoo.fr>
2016-01-14 22:08:24 +0100

    - ajout fichier

---
+++ ECU/Trigger_Input.cpp
@@ -0,0 +1,4 @@
+#include "Trigger_Input.h"
+
+
+
---
+++ ECU/Trigger_Input.h
@@ -0,0 +1,19 @@
+#ifndef _TRIGGER_INPUT_H_
+#define _TRIGGER_INPUT_H_
+
+
+class TriggerInput
+{
+   public:
+   //****************************
+   //  Configuration
+   //****************************
+   
+   private:
+   // properties
+   static unsigned short pwmPeriod;
+   static unsigned char clockSelectBits;
+   
+}
+#endif
+
